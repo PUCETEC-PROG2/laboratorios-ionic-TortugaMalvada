b1f60ea6521e3b301288b6a5ab44882330d42112
Jhoan Alvarez <djalvarezv@puce.edu.ec>
2026-06-20 18:49:21 -0500

    Taller en clase

--- src/components/RepoItem.tsx
+++ src/components/RepoItem.tsx
@@ -1,31 +1,48 @@
 import './RepoItem.css';
 import React from 'react';
-import { IonItemSliding, IonItem, IonThumbnail, IonLabel, IonItemOptions, IonItemOption, IonIcon } from '@ionic/react';
+import {
+  IonItemSliding,
+  IonItem,
+  IonThumbnail,
+  IonLabel,
+  IonItemOptions,
+  IonItemOption,
+  IonIcon
+} from '@ionic/react';
 import { pencil, trash } from 'ionicons/icons';
+import { Repository } from '../interfaces/Repository';
 
-const RepoItem: React.FC<any> = ({ ...repository }) => {
-    return (
-        <IonItemSliding>
-            <IonItem>
-                <IonThumbnail slot="start">
-                <img src={repository.owner.avatar_url} alt="Avatar"/>
-                </IonThumbnail>
-                <IonLabel>
-                <h2>{repository.name}</h2>
-                <p>{repository.description}</p>
-                <p><strong>Lenguaje:</strong> {repository.language}</p>
-                </IonLabel>
-            </IonItem>
-            <IonItemOptions>
-                <IonItemOption>
-                <IonIcon icon={pencil} slot="icon-only" />
-                </IonItemOption>
-                <IonItemOption color="danger">
-                <IonIcon icon={trash} slot="icon-only" />
-                </IonItemOption>
-            </IonItemOptions>
-        </IonItemSliding>
-    );
-}
+const RepoItem: React.FC<Repository> = ({
+  name,
+  description,
+  language,
+  owner
+}) => {
+  return (
+    <IonItemSliding>
+      <IonItem>
+        <IonThumbnail slot="start">
+          <img src={owner.avatar_url} alt="Avatar" />
+        </IonThumbnail>
+
+        <IonLabel>
+          <h2>{name}</h2>
+          <p>{description}</p>
+          <p><strong>Lenguaje:</strong> {language}</p>
+        </IonLabel>
+      </IonItem>
+
+      <IonItemOptions>
+        <IonItemOption>
+          <IonIcon icon={pencil} slot="icon-only" />
+        </IonItemOption>
+
+        <IonItemOption color="danger">
+          <IonIcon icon={trash} slot="icon-only" />
+        </IonItemOption>
+      </IonItemOptions>
+    </IonItemSliding>
+  );
+};
 
 export default RepoItem;
--- src/services/GithunSevices.tsx
+++ src/services/GithunSevices.tsx
@@ -1,31 +1,29 @@
 import axios from 'axios';
 import { Repository } from '../interfaces/Repository';
 
-const GITHUB_API_URL = import.meta.env.VITE_GITHUB_API_URL;
-const GITHUB_API_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
+console.log(import.meta.env.VITE_GITHUB_TOKEN);
+console.log(import.meta.env.VITE_GITHUB_API_TOKEN);
 
 export const fetchRepositories = async (): Promise<Repository[]> => {
   try {
-    const response = await axios.get<Repository[]>(
-      `${GITHUB_API_URL}/user/repos`,
-      {
+    const response = await axios.get(`${import.meta.env.VITE_GITHUB_API_URL}/user/repos`,{
         headers: {
-          Authorization: `Bearer ${GITHUB_API_TOKEN}`,
+          Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
         },
+        params: {
+          sort: 'created',
+          per_page: 100,
+          direction: 'desc',
+          affiliation: 'owner',
+          t: Date.now() // Cache busting parameter
+        },
+      });
+      if (response.status !== 200) {
+        throw new Error(`Error obteniendo repositorios: ${response.statusText}`);
       }
-    );
-
     return response.data;
   } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.error(
-        'Error obteniendo repositorios:',
-        error.response?.data || error.message
-      );
-    } else {
-      console.error('Error desconocido:', error);
-    }
-
-    return [];
+      console.error('Error obteniendo repositorios:', error);
+      return [];
   }
 };
